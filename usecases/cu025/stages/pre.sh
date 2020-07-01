3c0f77e85ade95ff1d7fa18c56ea8cca03df183b
Luis Guillen Civera <luisguillenc@gmail.com>
2020-07-01 19:54:09 +0200

    updated tlsnotary uses cases

--- usecases/cu025/stages/pre.sh
+++ usecases/cu025/stages/pre.sh
@@ -29,7 +29,6 @@ prepare_workdir() {
     ##config tlsnotary
     mkdir -p $ETCDIR/luids/notary || return $?
     cp $DATADIR/tlsnotary.toml $ETCDIR/luids/notary || return $?
-    cp $DATADIR/verifiers.json $ETCDIR/luids/notary || return $?
 }
 
 create_workdir || die "creating workdir"
